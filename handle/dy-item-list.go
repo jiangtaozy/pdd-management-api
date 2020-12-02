c895e75c2c6d679d783227831facf41937acaa4f
jemo <1321332562@qq.com>
2020-12-02 14:03:05 +0800

    douyin item

--- handle/dy-item-list.go
+++ handle/dy-item-list.go
@@ -8,20 +8,120 @@
 package handle
 
 import (
+  "log"
   "net/http"
-  //"encoding/json"
-  //"github.com/jiangtaozy/openapi-fxg"
+  "encoding/json"
+  "database/sql"
+  "github.com/jiangtaozy/pdd-management-api/database"
 )
 
 func DyItemList(w http.ResponseWriter, r *http.Request) {
-  // get access token
-  //shopId := "973906"
-  //accessToken := GetAccessToken(shopId)
-  //itemList := openapiFxg.ProductList(
-  //  appId,
-  //  appSecret,
-  //  accessToken,
-  //)
-  //json.NewEncoder(w).Encode(itemList)
+  db := database.DB
+  rows, err := db.Query(`
+    SELECT
+      checkStatus,
+      createTime,
+      discountPrice,
+      extra,
+      firstCid,
+      img,
+      marketPrice,
+      mobile,
+      name,
+      outProductId,
+      payType,
+      productId,
+      productIdStr,
+      recommendRemark,
+      secondCid,
+      settlementPrice,
+      specId,
+      status,
+      thirdCid,
+      updateTime
+    FROM
+      dyItem
+  `)
+  if err != nil {
+    log.Println("dy-item-list-query-error: ", err)
+    http.Error(w, err.Error(), 500)
+    return
+  }
+  defer rows.Close()
+  var list []interface{}
+  for rows.Next() {
+    var (
+      checkStatus int64
+      createTime sql.NullString
+      discountPrice sql.NullInt64
+      extra sql.NullString
+      firstCid sql.NullInt64
+      img sql.NullString
+      marketPrice sql.NullInt64
+      mobile sql.NullString
+      name sql.NullString
+      outProductId sql.NullInt64
+      payType sql.NullInt64
+      productId sql.NullInt64
+      productIdStr sql.NullString
+      recommendRemark sql.NullString
+      secondCid sql.NullInt64
+      settlementPrice sql.NullInt64
+      specId sql.NullInt64
+      status sql.NullInt64
+      thirdCid sql.NullInt64
+      updateTime sql.NullString
+    )
+    err = rows.Scan(
+      &checkStatus,
+      &createTime,
+      &discountPrice,
+      &extra,
+      &firstCid,
+      &img,
+      &marketPrice,
+      &mobile,
+      &name,
+      &outProductId,
+      &payType,
+      &productId,
+      &productIdStr,
+      &recommendRemark,
+      &secondCid,
+      &settlementPrice,
+      &specId,
+      &status,
+      &thirdCid,
+      &updateTime,
+    )
+    if err != nil {
+      log.Println("dy-item-list-scan-error: ", err)
+      http.Error(w, err.Error(), 500)
+      return
+    }
+    item := map[string]interface{}{
+      "checkStatus": checkStatus,
+      "createTime": createTime.String,
+      "discountPrice": discountPrice.Int64,
+      "extra": extra.String,
+      "firstCid": firstCid.Int64,
+      "img": img.String,
+      "marketPrice": marketPrice.Int64,
+      "mobile": mobile.String,
+      "name": name.String,
+      "outProductId": outProductId.Int64,
+      "payType": payType.Int64,
+      "productId": productId.Int64,
+      "productIdStr": productIdStr.String,
+      "recommendRemark": recommendRemark.String,
+      "secondCid": secondCid.Int64,
+      "settlementPrice": settlementPrice.Int64,
+      "specId": specId.Int64,
+      "status": status.Int64,
+      "thirdCid": thirdCid.Int64,
+      "updateTime": updateTime.String,
+    }
+    list = append(list, item)
+  }
+  json.NewEncoder(w).Encode(list)
 }
-
